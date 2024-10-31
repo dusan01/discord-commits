b1e7326a289bdbd09472db93bfddb817f43a1ca4
shanruto <dalavanja025@gmail.com>
2024-10-31 12:57:43 +0100

    Update discord.js

--- src/discord.js
+++ src/discord.js
@@ -1,53 +1,52 @@
 const { MessageEmbed, WebhookClient } = require('discord.js')
 const MAX_MESSAGE_LENGTH = 72
 
-module.exports.send = (id, token, repo, url, commits, size, pusher) =>
+module.exports.send = (id, token, repo, url, commits, size, pusher, branch) =>
   new Promise((resolve, reject) => {
-    let client
-    const username = repo.replace(/(discord)/gi, '******')
-    console.log('Preparing Webhook...')
+    let client;
+    const username = repo.replace(/(discord)/gi, '******');
+    console.log('Preparing Webhook...');
     try {
       client = new WebhookClient({
         id: id,
         token: token,
-      })
+      });
       client
         .send({
           username: username,
-          embeds: [createEmbed(url, commits, size, pusher)],
+          embeds: [createEmbed(url, commits, size, pusher, branch)],
         })
         .then(() => {
-          console.log('Successfully sent the message!')
-          resolve()
-        }, reject)
+          console.log('Successfully sent the message!');
+          resolve();
+        }, reject);
     } catch (error) {
-      console.log('Error creating Webhook')
-      reject(error.message)
-      return
+      console.log('Error creating Webhook');
+      reject(error.message);
+      return;
     }
-  })
+  });
 
-function createEmbed(url, commits, size, pusher) {
-  console.log('Constructing Embed...')
-  console.log('Commits :')
-  console.log(commits)
+function createEmbed(url, commits, size, pusher, branch) {
+  console.log('Constructing Embed...');
+  console.log('Commits :');
+  console.log(commits);
   if (!commits) {
-    console.log('No commits, skipping...')
-    return
+    console.log('No commits, skipping...');
+    return;
   }
-  const latest = commits[0]
+  const latest = commits[0];
   return new MessageEmbed()
     .setColor(0xff3461)
     .setAuthor({
-      name: `⚡ ${pusher} pushed ${size} commit${
-        size === 1 ? '' : 's'
-      }`,
+      name: `⚡ ${pusher} pushed ${size} commit${size === 1 ? '' : 's'} to ${branch}`,
       iconURL: `https://github.com/${pusher}.png?size=64`,
       url: url,
     })
     .setDescription(`${getChangeLog(commits, size)}`)
-    .setTimestamp(Date.parse(latest.timestamp))
+    .setTimestamp(Date.parse(latest.timestamp));
 }
+
 
 function getChangeLog(commits, size) {
   let changelog = ''
