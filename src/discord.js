b1e0a7249e6ae062176011dd2e2b628d301598a4
shanruto <dalavanja025@gmail.com>
2024-10-31 13:08:48 +0100

    Update discord.js

--- src/discord.js
+++ src/discord.js
@@ -1,5 +1,5 @@
-const { MessageEmbed, WebhookClient } = require('discord.js');
-const MAX_MESSAGE_LENGTH = 72;
+const { MessageEmbed, WebhookClient } = require('discord.js')
+const MAX_MESSAGE_LENGTH = 72
 
 module.exports.send = (id, token, repo, url, commits, size, pusher, branch) =>
   new Promise((resolve, reject) => {
@@ -39,30 +39,31 @@ function createEmbed(url, commits, size, pusher, branch) {
   return new MessageEmbed()
     .setColor(0xff3461)
     .setAuthor({
-      name: `⚡ ${pusher} pushed ${size} commit${size === 1 ? '' : 's'} to ${branch}`,
+      name: `⚡ ${pusher} pushed ${size} commit${size === 1 ? '' : 's'} (Branch: ${branch})`,
       iconURL: `https://github.com/${pusher}.png?size=64`,
       url: url,
     })
-    .setDescription(`${getChangeLog(commits, size, branch)}`)
+    .setDescription(`${getChangeLog(commits, size)}`)
     .setTimestamp(Date.parse(latest.timestamp));
 }
 
-function getChangeLog(commits, size, branch) {
-  let changelog = '';
+
+function getChangeLog(commits, size) {
+  let changelog = ''
   for (const i in commits) {
     if (i > 7) {
-      changelog += `+ ${size - i} more...\n`;
-      break;
+      changelog += `+ ${size - i} more...\n`
+      break
     }
 
-    const commit = commits[i];
-    const sha = commit.id.substring(0, 6);
+    const commit = commits[i]
+    const sha = commit.id.substring(0, 6)
     const message =
       commit.message.length > MAX_MESSAGE_LENGTH
         ? commit.message.substring(0, MAX_MESSAGE_LENGTH) + '...'
-        : commit.message;
-    changelog += `Branch: ${branch}\n[\`${sha}\`](${commit.url}) — ${message} ([\`${commit.author.username}\`](https://github.com/${commit.author.username}))\n`;
+        : commit.message
+    changelog += `[\`${sha}\`](${commit.url}) — ${message} ([\`${commit.author.username}\`](https://github.com/${commit.author.username}))\n`
   }
 
-  return changelog;
+  return changelog
 }
