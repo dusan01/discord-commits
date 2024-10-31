dcd0bf8dad6745db36d15b92ba45b319fa3a95d3
shanruto <dalavanja025@gmail.com>
2024-10-31 13:03:10 +0100

    Update discord.js

--- src/discord.js
+++ src/discord.js
@@ -62,7 +62,7 @@ function getChangeLog(commits, size) {
       commit.message.length > MAX_MESSAGE_LENGTH
         ? commit.message.substring(0, MAX_MESSAGE_LENGTH) + '...'
         : commit.message
-    changelog += `[\`${sha}\`](${commit.url}) — ${message} ([\`${commit.author.username}\`](https://github.com/${commit.author.username}))\n`
+    changelog += `[\`${sha}\`](${commit.url}) — ${message} ([\`${commit.author.username}\`](https://github.com/${commit.author.username}))\n Test`
   }
 
   return changelog
